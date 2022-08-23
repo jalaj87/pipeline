b6ccf1ca25df70465b01c863066f375e283878a9
Jalaj Kumar <Jalaj.kumar1987@gmail.com>
2022-08-23 11:56:11 -0400

    Update CreateComponent.jsx

--- src/Components/Pipeline/CreateComponent.jsx
+++ src/Components/Pipeline/CreateComponent.jsx
@@ -7,7 +7,7 @@ import Row from 'react-bootstrap/Row';
 
 export default function App() {
   return (
-    <div className="mb-4 align-middle" style={{display: 'block', width: 1300, 
+    <div className="mb-4 align-middle" style={{display: 'block', width: 1500, 
                   padding: 80}}>
       <h4>Creating Java-Maven Pipeline</h4>
       <Form>
@@ -39,9 +39,9 @@ export default function App() {
           <Col sm={10}>
           <Form.Select>
               <option>Choose the Controller</option>
-                <option value="ctrl1">ctrl1</option>
-                <option value="ctrl2">ctrl2</option>
-                <option value="VS">ctrl3</option>
+                <option value="ctrl1">Windows</option>
+                <option value="ctrl2">Linux</option>
+                <option value="ctrl3">Docker</option>
           </Form.Select>
           </Col>
         </Form.Group>
@@ -52,8 +52,8 @@ export default function App() {
           <Form.Select>
               <option>Choose Application ID</option>
                 <option value="1">123</option>
-                <option value="2">234</option>
-                <option value="4">345</option>
+                <option value="2">456</option>
+                <option value="4">789</option>
           </Form.Select>
           </Col>
         </Form.Group>
@@ -64,8 +64,8 @@ export default function App() {
           <Form.Select>
               <option>Choose Deployment Process</option>
                 <option value="1">123</option>
-                <option value="2">234</option>
-                <option value="4">345</option>
+                <option value="2">456</option>
+                <option value="4">789</option>
           </Form.Select>
  </Col>
         </Form.Group>
